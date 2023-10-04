ec84b9c1a1de1ba045e04ef228595c47d7ce00c3
Gufran Afzal <gufranafzal54@gmail.com>
2023-10-04 09:05:34 +0500

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './assets/*.liquid',
+    './layout/*.liquid',
+    './snippets/*.liquid',
+    './sections/*.liquid',
+    './templates/customers/*.liquid',
+    './templates/*.liquid'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
